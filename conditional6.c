51f173b1a8fbddd482afb24e36511e8b026c2462
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-01-26 09:59:47 +0530

    Add files via upload

---
+++ conditional6.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+int main(){
+    char A;
+    scanf("%c",&A);
+    if(A=='a'||A=='e'||A=='i'||A=='o'||A=='u'){
+      printf("Vowel");}
+    else
+      printf("Consonants");
+       
+    return 0;
+}
