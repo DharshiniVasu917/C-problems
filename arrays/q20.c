83fb89545621b3b0986bf47571184bc4eb59972c
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-02-03 09:11:42 +0530

    Add files via upload

---
+++ arrays/q20.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int size;
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<size;i++){
+        if(arr[i]<0){
+         printf("0");
+        }
+        else{
+            printf("%d ",arr[i]);
+        }
+
+    }
+    return 0;
+
+}
