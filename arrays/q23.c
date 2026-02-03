4e89654a60562450d90967f81b783884fb40f202
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-02-03 22:08:18 +0530

    Add files via upload

---
+++ arrays/q23.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int size,min=0;
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=1;i<size;i++){
+
+    }
+}
