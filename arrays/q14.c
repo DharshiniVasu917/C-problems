87b062e1278dc629780dfa3002a46cd57b797ac0
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-02-03 09:06:48 +0530

    Add files via upload

---
+++ arrays/q14.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int size,min=-1;
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int j=0;j<size;j++){
+        if(arr[j]>0){
+            if(min==-1||arr[j]<min)        
+               min=arr[j];
+        }
+
+    }
+    if(min==-1){
+        printf("No positive");
+    }
+    else{
+    printf("%d",min);}
+    return 0;
+}
