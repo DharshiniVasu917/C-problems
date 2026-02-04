9261d04d5a4f8d9d8a3356268581a364cfec325d
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-02-04 20:05:48 +0530

    Add files via upload

--- arrays/q23.c
+++ arrays/q23.c
@@ -1,12 +1,22 @@
 #include<stdio.h>
 int main(){
-    int size,min=0;
+    int size,min=-1;
     scanf("%d",&size);
     int arr[size];
     for(int i=0;i<size;i++){
         scanf("%d",&arr[i]);
     }
-    for(int i=1;i<size;i++){
+   for(int j=0;j<size;j++){
+        if(arr[j]>0){
+            if(min==-1||arr[j]<min)        
+               min=arr[j];
+        }
 
     }
+    if(min==-1){
+        printf("-1");
+    }
+    else{
+    printf("%d",min);}
+    return 0;
 }
