4b1ae4f53929a8df13cc1b50494fe6b7f4268f94
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-02-07 14:18:36 +0530

    Add files via upload

---
+++ 2Darray/q19.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d%d",&r,&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+   int unique=0;
+    for(int i=0;i<r;i++){
+        int dup=0,count=0;
+        for(int j=0;j<c;j++){
+          for(int k=j+1;k<c;k++){
+            if(arr[i][j]==arr[i][k])
+            dup=1;
+            break;
+          }
+          if(dup)break;
+        }
+          if(!dup){
+            unique++;
+          }
+    }
+    printf("%d",unique);
+    return 0;
+}
