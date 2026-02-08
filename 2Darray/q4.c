4847f08003aa147bd003c23dbf7430f8edf6d4d7
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-02-08 09:09:44 +0530

    Add files via upload

---
+++ 2Darray/q4.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d%d",&r,&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int ref=0;
+    for(int i=0;i<r;i++){
+
+        for(int j=0;j<c;j++){
+            int occ=0;
+            int current=arr[i][j];
+            for(int x=0;x<r;x++){
+                for(int y=0;y<c;y++){
+                    if(arr[x][y]==current){
+                        occ++;
+                    }
+                }
+            }
+            
+            if(occ>1){
+                printf("%d",current);
+               ref=1;
+               break;
+            }
+          
+          } 
+           if(ref) break;
+        }
+     
+    
+
+    return 0;
+}
