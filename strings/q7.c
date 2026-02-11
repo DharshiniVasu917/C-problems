129d23b15236b6396b4ff722149a338d95b94f94
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-02-11 16:42:35 +0530

    Add files via upload

---
+++ strings/q7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    int n=0,count=0;
+    while(str[n]!='\0'){
+        if(str[n]!='a'&& str[n]!='e'&& str[n]!='i'&& str[n]!='o'&& str[n]!='u'&& str[n]!='A'&& str[n]!='E'&& str[n]!='I'&& str[n]!='O'&& str[n]!='U')
+        if((str[n]>='a' && str[n]<='z')||(str[n]>='A' && str[n]<='Z')){
+         count++;}
+        n++;
+    }
+    printf("%d",count);
+}
