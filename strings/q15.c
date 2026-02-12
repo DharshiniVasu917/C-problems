d6d69a57b8d2b488fa59c5279001bdb9b5225209
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-02-12 06:03:31 +0530

    Add files via upload

---
+++ strings/q15.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    int n=0,count=0;
+    while(str[n]!='\0'){
+        if(str[n]==' '){
+            count++;
+        }
+        n++;
+    }
+    printf("%d",count+1);
+}
